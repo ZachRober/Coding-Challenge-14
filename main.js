f86ef9d9915c71e6efce45af56325bc1e3b4ef3a
ZachRober <nolethedgehogdie@gmail.com>
2024-10-28 19:22:46 -0400

    everything but finally complete

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.getElementById('container');
+const myContainer = document.getElementById('container');
 
 async function fetchData() {
     try{
@@ -12,9 +12,24 @@ async function fetchData() {
         if(data.length<1){
             throw new Error("No Support Tickets")
         }
+        for (let i = 0; i < data.length; i++) {
+            let ticketID = data[i].id;
+            let customerName = data[i].userId;
+            let issueDescription = data[i].title;
+            let details = data[i].body;
+            let newTicket = document.createElement('p');
+            newTicket.textContent = `Ticket ID:${ticketID} User ID:${customerName} Description:${issueDescription} Details:${details}`;
+            myContainer.append(newTicket);
+            
+        }
     }
     catch(error){
         console.error(error);
     }
+    finally{
+
+    }
+    
+    
 }
 fetchData();
